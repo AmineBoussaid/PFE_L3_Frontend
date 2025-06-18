2e64ed18f3274b84cb6dbf7f8547b1c26a1aad7e
Soufiane <soufiane.boussaid@gmail.com>
2025-06-18 22:05:17 +0000

    first-commit

---
+++ src/app/services/login.service.ts
@@ -0,0 +1,15 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class LoginService {
+
+  constructor(private http:HttpClient) { }
+
+  getLogin(username: string,password: string): Observable<{ success: boolean }>{
+    return this.http.post<{ success: boolean }>('/api/login', { username, password });
+  }
+}
